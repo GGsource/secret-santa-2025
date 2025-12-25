e506e77683f608bd124bef94f7dcd88726c5c277
GGMachina <git@ggsource.dev>
2025-12-25 12:28:06 -0600

    Uploaded files for Github Pages Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	base: "/",
 	server: {
 		open: true, // This opens the browser automatically
+	},
+	build: {
+		assetsInlineLimit: 0,
 	},
 });
