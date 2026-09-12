5658b69ecd10dc26631849d3923e558edf040015
GGMachina <git@ggsource.dev>
2026-09-12 02:44:14 -0500

    Removed 404 mention from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, "index.html"),
-				notFound: resolve(__dirname, "404.html"),
 			},
 		},
 		assetsInlineLimit: 0,
